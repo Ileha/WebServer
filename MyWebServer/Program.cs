3f611d8bb146b1e6ddf09fd86646347bd9ecbed1
Ileha <i.leha98@mail.ru>
2017-11-07 07:42:40 +0300

    work module loader to domain

--- MyWebServer/Program.cs
+++ MyWebServer/Program.cs
@@ -12,26 +12,22 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Runtime.Remoting;
 using System.Security.Policy;
-//using UsI;
 
 namespace MainName {
 
 	public class MyClass : MarshalByRefObject {
-		public Assembly GetAssemblyByName(string assemblyName) {
-			try {
-				return Assembly.Load(assemblyName);
-			}
-			catch (Exception) {
-				return null;
-			}
+		public void GetAssemblyByName(string assemblyName) {
+			Assembly.Load(assemblyName);
 		}
 		public void LoadPluginFrom() {
 			FileInfo[] files = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).GetFiles("*.dll");
 			foreach (FileInfo fi in files)
 			{
-			    // Получаем assemly из файла
-			    Assembly.LoadFile(fi.FullName);
+				Console.WriteLine("loading " + fi.FullName);
+				// Получаем assemly из файла
+				Assembly.LoadFrom(fi.FullName);
 			    // Ищем нужный тип
+				Console.WriteLine("load");
 			}
 		}
 		public void Info() {
@@ -53,12 +49,10 @@ namespace MainName {
 						typeof(MyClass).FullName);
 			
 
-            InfoDomainApp(AppDomain.CurrentDomain);
-			resident.Info();
+			//resident.Info();
 			//resident.GetAssemblyByName("system, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
 			resident.LoadPluginFrom();
 			//resident.GetAssembly(@"../../../add/bin/Debug/add.dll");
-            InfoDomainApp(domain);
 
 			//DirectoryInfo dir = new DirectoryInfo(domain.BaseDirectory);
 			//IEnumerable<FileInfo> fileNames = dir.GetFiles("*.dll", SearchOption.TopDirectoryOnly);
@@ -97,75 +91,5 @@ namespace MainName {
             //new WebSerwer(ipAddr, 11000, "/Users/Alexey/Documents/Programm Projects/C#/WebServer/Resourses");
             Console.ReadLine();
         }
-
-		static Assembly MyHandler(object source, ResolveEventArgs e)
-		{
-			Console.WriteLine("Resolving {0}", e.Name);
-			Console.WriteLine("Resolve!!!");
-			return Assembly.Load(e.Name);
-		}
-
-		private static IEnumerable<T> EnumerateExtensions<T>(AppDomain domain)
-		{
-			DirectoryInfo dir = new DirectoryInfo(domain.BaseDirectory);
-			Console.WriteLine(dir.FullName);
-			IEnumerable<FileInfo> fileNames = dir.GetFiles("*.dll", SearchOption.TopDirectoryOnly);
-			if (fileNames != null)
-			{
-				foreach (FileInfo assemblyFileName in fileNames)
-				{
-					foreach (string typeName in GetTypes(assemblyFileName.FullName, typeof(T), domain))
-					{
-						System.Runtime.Remoting.ObjectHandle handle;
-						try
-						{
-							handle = domain.CreateInstanceFrom(assemblyFileName.FullName, typeName);
-						}
-						catch (MissingMethodException)
-						{
-							continue;
-						}
-						object obj = handle.Unwrap();
-						T extension = (T)obj;
-						yield return extension;
-					}
-				}
-			}
-		}
-
-		static AppDomain CreateDomain(string path) {
-			AppDomainSetup setup = new AppDomainSetup();
-			setup.ApplicationBase = new DirectoryInfo(path).FullName;
-			return AppDomain.CreateDomain("Temporary domain", null, setup);
-		}
-
-		private static IEnumerable<string> GetTypes(string assemblyFileName, Type interfaceFilter, AppDomain domain) {
-			Console.WriteLine("loading {0}", assemblyFileName);
-			Assembly asm = domain.Load(AssemblyName.GetAssemblyName(assemblyFileName));
-			Type[] types = asm.GetTypes();
-			foreach (Type type in types)
-			{
-				//Console.WriteLine(type.ToString());
-				if (type.GetInterface(interfaceFilter.Name) != null)
-				{
-					yield return type.FullName;
-				}
-			}
-		}
-
-		static void InfoDomainApp(AppDomain defaultD)
-		{
-			Console.WriteLine("*** Информация о домене приложения ***\n");
-			Console.WriteLine("-> Имя: {0}\n-> ID: {1}\n-> По умолчанию? {2}\n-> Путь: {3}\n",
-				defaultD.FriendlyName, defaultD.Id, defaultD.IsDefaultAppDomain(), defaultD.BaseDirectory);
-
-			Console.WriteLine("Загружаемые сборки: \n");
-			// Извлекаем информацию о загружаемых сборках с помощью LINQ-запроса
-			var infAsm = defaultD.GetAssemblies();
-			foreach (var a in infAsm) {
-				//Console.WriteLine("-> Имя: \t{0}\n-> Версия: \t{1}", a.GetName().Name, a.GetName().Version);
-				Console.WriteLine(a.FullName);
-			}
-		}
     }
 }
