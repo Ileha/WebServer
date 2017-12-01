7bd82da36fee0d5a3d0db8eeda6b50d7a4662a58
Ileha <i.leha98@mail.ru>
2017-12-01 23:29:49 +0300

    Adding directory for modules in config

--- MyWebServer/Program.cs
+++ MyWebServer/Program.cs
@@ -30,14 +30,21 @@ namespace MainProgramm {
             foreach (XElement host_conf in config_doc.Root.Elements())
             {
                 AppDomainSetup domaininfo = new AppDomainSetup();
-                domaininfo.ApplicationBase = @"../../../HTTPHandlers/bin/Debug";
+				bool has_modules = true;
+				try {
+					domaininfo.ApplicationBase = host_conf.Element("modules_dir").Value;
+				}
+				catch (Exception err) {
+					domaininfo.ApplicationBase = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+					has_modules = false;
+				}
                 Evidence adevidence = AppDomain.CurrentDomain.Evidence;
-                AppDomain domain = AppDomain.CreateDomain("test_domain", adevidence, domaininfo);
+                AppDomain domain = AppDomain.CreateDomain(host_conf.Element("name").Value, adevidence, domaininfo);
                 Resident resident = (Resident)domain.CreateInstanceAndUnwrap(
                             typeof(Resident).Assembly.FullName,
                             typeof(Resident).FullName);
                 resident.AddConfig(doc_path, i);
-                resident.LoadPluginExternal();
+				if (has_modules) resident.LoadPluginExternal();
                 resident.LoadPluginInternal();
                 resident.GetPluginInfo();
                 resident.StartHost();
