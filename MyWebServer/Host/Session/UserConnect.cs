e8626b05dc7d0e7e43bfca188c15dce6c8eca2a6
Ileha <i.leha98@mail.ru>
2017-12-25 12:39:25 +0300

    add session

--- MyWebServer/Host/Session/UserConnect.cs
+++ MyWebServer/Host/Session/UserConnect.cs
@@ -6,6 +6,7 @@ namespace Host.Session {
 	public class UserConnect
 	{
 		private static Dictionary<string, UserConnect> SessionInfo;
+        private static Queue<UserConnect> deleting_queue;
 		private static long _long_live;
 		private static Timer t;
 
@@ -14,7 +15,8 @@ namespace Host.Session {
 			_long_live = Convert.ToInt64(Repository.Configurate["session_collector"].Attribute("time_of_life").ToString());
 			TimerCallback time = new TimerCallback(Collect);
 			t = new Timer(time, null, 0, Convert.ToInt32(Repository.Configurate["session_collector"].Attribute("time_of_collect").ToString()));
-		}
+            deleting_queue = new Queue<UserConnect>();
+        }
 		public static UserConnect GetUserDataFromID(string id) {
 			try {
 				UserConnect session = SessionInfo[id];
@@ -27,22 +29,31 @@ namespace Host.Session {
 		}
 
 		private static void Collect(object obj) {
-			foreach (UserConnect usr in SessionInfo.Values) {
-				
-			}
+            Console.WriteLine(Repository.Configurate["name"].Value+" tick !!!");
+            if (deleting_queue.Count == 0) { return; }
+            UserConnect curr_delete = deleting_queue.Peek();
+            while (curr_delete.timeOfLife.CompareTo(DateTime.Now) == -1) {
+                SessionInfo.Remove(curr_delete.ID);
+                deleting_queue.Dequeue();
+                if (deleting_queue.Count == 0) { break; }
+                curr_delete = deleting_queue.Peek();
+            }
 		}
 
-
-		public string ID { get; }
+        private string _id;
+        public string ID {
+            get { return _id; }
+        }
 		private Dictionary<string, object> Data;
 		private DateTime timeOfLife;
 
 		public UserConnect()
 		{
-			ID = Guid.NewGuid().ToString("N");
+			_id = Guid.NewGuid().ToString("N");
 			Data = new Dictionary<string, object>();
 			SetDeletingTime();
 			SessionInfo.Add(ID, this);
+            deleting_queue.Enqueue(this);
 		}
 		private void SetDeletingTime() {
 			timeOfLife = new DateTime(DateTime.Now.Ticks+10000*_long_live);
