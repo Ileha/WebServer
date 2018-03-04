82422bd1d3013192aa495943e1b3d770b6f9a04f
Ileha <i.leha98@mail.ru>
2018-03-04 10:56:53 +0300

    remake of header data parser

--- MyWebServer/Host/HeaderData/HeaderValueMain.cs
+++ MyWebServer/Host/HeaderData/HeaderValueMain.cs
@@ -4,28 +4,36 @@ using System.Text.RegularExpressions;
 
 namespace Host.HeaderData
 {
-	public class HeaderValue {
+	public class HeaderValue
+	{
 		private static Regex eq = new Regex("=");
-		private static Regex simple = new Regex("(?<name>[^=]+)=(?<val>[^=]+)");
+		private static Regex simple = new Regex("(?<name>[^=]+)=(?<val>.+)");
 
-		public Dictionary<string, string> Value {
-			get {
+		public Dictionary<string, string> Value
+		{
+			get
+			{
 				return _value;
 			}
 		}
 		private Dictionary<string, string> _value;
 
-		public HeaderValue(string pars_string) {
+		public HeaderValue(string pars_string)
+		{
 			_value = new Dictionary<string, string>();
-			string[] vals = Regex.Split(pars_string, ";");
+			string[] vals = Regex.Split(pars_string, ";|\\s");
 			int i = 0;
-			foreach (string str in vals) {
-				if (str != "") {
-					if (eq.IsMatch(str)) {
+			foreach (string str in vals)
+			{
+				if (str != "")
+				{
+					if (eq.IsMatch(str))
+					{
 						Match match = simple.Match(str);
 						Value.Add(match.Groups["name"].Value.Trim(), match.Groups["val"].Value);
 					}
-					else {
+					else
+					{
 						Value.Add(i.ToString(), str);
 					}
 					i++;
@@ -34,57 +42,33 @@ namespace Host.HeaderData
 		}
 	}
 
-	public class HeaderValueMain {
-		private static Regex first = new Regex("(?<name>[^:]+):[ ]?(?<val>[^,]+)");
-		private static Regex eq = new Regex("=");
-		private static Regex simple = new Regex("(?<name>[^=]+)=(?<val>[^=]+)");
+	public class HeaderValueMain
+	{
+		private static Regex first = new Regex("(?<name>[^:]+):[ ]?(?<val>.+)");
 
 		public string Name;
-		public Dictionary<string, string> Value { 
-			get {
+		public List<HeaderValue> Value
+		{
+			get
+			{
 				return _value;
-			} 
+			}
 		}
-		private Dictionary<string, string> _value;
+		private List<HeaderValue> _value;
 
-		private HeaderValue[] bank;
-
-		public HeaderValueMain(string pars_string) {
-			_value = new Dictionary<string, string>();
+		public HeaderValueMain(string pars_string)
+		{
+			_value = new List<HeaderValue>();
 			Match m = first.Match(pars_string);
-			string after = pars_string.Substring(m.Length);
 			Name = m.Groups["name"].Value;
-			string[] vals = Regex.Split(m.Groups["val"].Value, ";");
-			int i = 0;
-			foreach (string str in vals) {
-				if (str != "") {
-					if (eq.IsMatch(str)) {
-						Match match = simple.Match(str);
-						Value.Add(match.Groups["name"].Value.Trim(), match.Groups["val"].Value);
-					}
-					else {
-						Value.Add(i.ToString(), str);
-					}
-					i++;
+			string[] vals = Regex.Split(m.Groups["val"].Value, ",");
+			foreach (string str in vals)
+			{
+				if (str != "")
+				{
+					_value.Add(new HeaderValue(str));
 				}
 			}
-			if (after.Length == 0) { return; }
-			vals = Regex.Split(after, ",");
-			i = 0;
-			foreach (string str in vals) {
-				if (str != "") { i++; }
-			}
-			bank = new HeaderValue[i];
-			foreach (string str in vals) {
-				if (str != "") {
-					bank[bank.Length - i] = new HeaderValue(str);
-					i--;
-				}
-			}
-		}
-
-		public HeaderValue[] GetAny() {
-			return bank;
 		}
 	}
 }
