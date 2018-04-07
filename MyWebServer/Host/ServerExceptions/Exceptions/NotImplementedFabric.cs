f19486d6bbf55da234207b92291556702cf10e02
Ileha <i.leha98@mail.ru>
2018-04-07 11:01:43 +0300

    add 501 exception

--- MyWebServer/Host/ServerExceptions/Exceptions/NotImplementedFabric.cs
+++ MyWebServer/Host/ServerExceptions/Exceptions/NotImplementedFabric.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-namespace MyWebServer
-{
-	public class NotImplementedFabric
-	{
-		public NotImplementedFabric()
-		{
+
+namespace Host.ServerExceptions {
+	public class NotImplementedFabric : ExceptionFabric {
+		public NotImplementedFabric() {}
+
+		public override string name { get { return "Not Implemented"; } }
+
+		public override ExceptionCode Create(ExceptionUserCode userCode, object data) {
+			return new NotImplemented(userCode, (string)data);
+		}
+	}
+
+	public class NotImplemented  : ExceptionCode {
+
+		public NotImplemented(ExceptionUserCode userCode, string message) : base(userCode) {
+			Code = "501 Not Implemented";
+			_IsFatal = true;
 		}
 	}
 }
