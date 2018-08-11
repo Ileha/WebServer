687ee3c603015dd8f09013aeb06b2adfb9673eb0
Ileha <i.leha98@mail.ru>
2018-08-11 13:27:20 +0300

    move libraries to folder

--- MyWebServer/Program.cs
+++ MyWebServer/Program.cs
@@ -148,34 +148,7 @@ namespace MainProgramm {
                 catch(CommandNotFound err) {
                     Console.WriteLine(err);
                 }
-                //StringOIInterface host = hosts[cmd_name[0]];
-                //host.Write(res[1]);
             }
-
-            //foreach (XElement host_conf in config_doc.Root.Elements())
-            //{
-            //    AppDomainSetup domaininfo = new AppDomainSetup();
-            //    bool has_modules = true;
-            //    try
-            //    {
-            //        domaininfo.ApplicationBase = host_conf.Element("modules_dir").Value;
-            //    }
-            //    catch (Exception err)
-            //    {
-            //        domaininfo.ApplicationBase = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            //        has_modules = false;
-            //    }
-            //    Evidence adevidence = AppDomain.CurrentDomain.Evidence;
-            //    AppDomain domain = AppDomain.CreateDomain(host_conf.Element("name").Value, adevidence, domaininfo);
-            //    Resident resident = (Resident)domain.CreateInstanceAndUnwrap(
-            //                typeof(Resident).Assembly.FullName,
-            //                typeof(Resident).FullName);
-            //    resident.AddConfig(host_conf.ToString());
-            //    if (has_modules) resident.LoadPluginExternal();
-            //    resident.LoadPluginInternal();
-            //    resident.GetPluginInfo();
-            //    resident.StartHost();
-            //}
             Console.ReadLine();
         }
     }
